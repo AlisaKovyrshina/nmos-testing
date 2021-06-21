78a599ff5e02ff0e01462e1dd51d215ee5d2b17c
Jonathan Thorpe (Sony) <Jonathan.Thorpe@sony.com>
2021-06-21 18:05:36 +0100

    Added mock node to the constructor Resources now have consistent flow and connection API urls Implemented Test_06 Identify which Receiver devices are controllable via IS-05

--- nmostesting/suites/JTNMTest.py
+++ nmostesting/suites/JTNMTest.py
@@ -66,23 +66,26 @@ class JTNMTest(GenericTest):
     """
     Testing initial set up of new test suite for controller testing
     """
-    def __init__(self, apis, registries, dns_server):
+    def __init__(self, apis, registries, node, dns_server):
         # JRT: remove the spec_path parameter to prevent GenericTest from attempting to download RAML from repo
         apis[JTNM_API_KEY].pop("spec_path", None)
         GenericTest.__init__(self, apis)
         self.authorization = False  # System API doesn't use auth, so don't send tokens in every request
         self.primary_registry = registries[1]
+        self.node = node
         self.dns_server = dns_server
         self.registry_mdns = []
         self.zc = None
         self.zc_listener = None
-        self.registry_location = ''
+        self.mock_registry_base_url = ''
+        self.mock_node_base_url = ''
         self.question_timeout = 600 # seconds
         self.test_data = self.load_resource_data()
         self.sender_possible_answers = [] # Reference for all possible senders
         self.sender_expected_answers = [] # Actual senders registered to mock registry
         self.receiver_possible_answers = [] # Reference for all possible receivers
         self.receiver_expected_answers = [] # Actual receivers registered to mock registry
+        self.connectable_receiver_expected_answers = [] # Receivers with connection api registered to mock registry (subset of receiver_expected_answers)
 
     def set_up_tests(self):
         self.zc = Zeroconf()
@@ -113,7 +116,8 @@ class JTNMTest(GenericTest):
         # Reset registry to clear previous heartbeats, etc.
         self.primary_registry.reset()
         self.primary_registry.enable()
-        self.registry_location = 'http://' + get_default_ip() + ':' + str(self.primary_registry.get_data().port) + '/'
+        self.mock_registry_base_url = 'http://' + get_default_ip() + ':' + str(self.primary_registry.get_data().port) + '/'
+        self.mock_node_base_url = 'http://' + get_default_ip() + ':' + str(self.node.port) + '/'
 
         if CONFIG.DNS_SD_MODE == "multicast":
             self.zc.register_service(self.registry_mdns[0])
@@ -124,7 +128,7 @@ class JTNMTest(GenericTest):
 
         self._populate_registry(MAX_SENDERS, MAX_RECEIVERS)
 
-        print('Registry should be available at ' + self.registry_location)
+        print('Registry should be available at ' + self.mock_registry_base_url)
 
 
     def tear_down_tests(self):
@@ -143,7 +147,7 @@ class JTNMTest(GenericTest):
         if self.dns_server:
             self.dns_server.reset()
 
-        self.registry_location = ''
+        self.mock_registry_base_url = ''
         self.registry_mdns = []
     
     def set_up_test(self):
@@ -271,20 +275,15 @@ class JTNMTest(GenericTest):
                            properties=txt, server=hostname)
         return info
 
-    def _randomise_indices(self, answer_count, max_choices=1):
+    def _generate_random_indices(self, index_range, min_index_count=2, max_index_count=4):
         """
-        answer_count: number of possible answers
-        max_choices: Maximum number of list indices to be returned. Default 1 for radio button answers. 
-        max_choices > 1 will return list containing a random number of list indices up to the value given
+        index_range: number of possible indices
+        min_index_count, max_index_count: Minimum, maximum number of indices to be returned. 
         """
-        answer_indices = []
-        if max_choices == 1:
-            answer_indices = [random.randint(0, answer_count-1)]
-        elif max_choices > 1:
-            choices = random.randint(1, max_choices)
-            answer_indices = random.sample(list(range(0, answer_count)), k=choices)
+        indices = list(range(index_range))
+        index_count = random.randint(min_index_count, max_index_count)
 
-        return answer_indices
+        return random.sample(indices, index_count)
 
     def _format_device_metadata(self, label, description, id):
         """ Used to format answers based on device metadata """
@@ -304,7 +303,7 @@ class JTNMTest(GenericTest):
 
         return answers
 
-    def _register_resources(self, type, labels, descriptions, resource_ids, indices):
+    def _register_resources(self, type, labels, descriptions, resource_ids, indices, include_connection_api=True):
         """
         type: of the resource e.g. sender, reciever
         labels: a list of resource labels
@@ -314,7 +313,7 @@ class JTNMTest(GenericTest):
         """
         # Post resources to registry
         for i in indices:
-            device_data = self.post_super_resources_and_resource(self, type, descriptions[i])
+            device_data = self.post_super_resources_and_resource(self, type, descriptions[i], include_connection_api)
             device_data['label'] = labels[i]
             resource_ids[i] = device_data['id'] # overwrite default UUID with actual one from the Registry
             self.post_resource(self, type, device_data, codes=[200])
@@ -330,7 +329,7 @@ class JTNMTest(GenericTest):
         sender_ids = [str(uuid.uuid4()), str(uuid.uuid4()), str(uuid.uuid4()), str(uuid.uuid4()), str(uuid.uuid4())]
         
         # Pick indices of up to max_sender_count senders to register
-        random_indices = self._randomise_indices(len(sender_labels), max_sender_count)
+        random_indices = self._generate_random_indices(len(sender_labels), min_index_count=1, max_index_count=4)
         sender_ids = self._register_resources("sender", sender_labels, sender_descriptions, sender_ids, random_indices)
 
         self.sender_expected_answers = self._generate_answers(sender_labels, sender_descriptions, sender_ids, random_indices);
@@ -340,13 +339,23 @@ class JTNMTest(GenericTest):
         receiver_labels = ['Test-node-2/receiver/palin', 'Test-node-2/receiver/cleese', 'Test-node-2/receiver/jones', 'Test-node-2/receiver/chapman', 'Test-node-2/receiver/idle', 'Test-node-2/receiver/gilliam']
         receiver_descriptions = ['Mock receiver 1', 'Mock receiver 2', 'Mock receiver 3', 'Mock receiver 4', 'Mock receiver 5', 'Mock receiver 6']
         receiver_ids = [str(uuid.uuid4()), str(uuid.uuid4()), str(uuid.uuid4()), str(uuid.uuid4()), str(uuid.uuid4()), str(uuid.uuid4())]
+        
+        # Pick random receivers to register
+        receiver_indices = self._generate_random_indices(len(receiver_labels), min_index_count=2, max_index_count=5);
 
-        # Pick up to max_receiver_count receivers to register
-        random_indices = self._randomise_indices(len(receiver_labels), max_receiver_count)
-        receiver_ids = self._register_resources("receiver", receiver_labels, receiver_descriptions, receiver_ids, random_indices)
+        # Split receiver_indices into those that have connection api and those that don't
+        connectable_receiver_count = random.randint(1, len(receiver_indices) - 1)
+        connectable_receiver_indices = receiver_indices[0:connectable_receiver_count]
+        not_connectable_receiver_indices = receiver_indices[connectable_receiver_count:len(receiver_indices)]
+        
+        # Register 'connectable' receivers
+        receiver_ids = self._register_resources("receiver", receiver_labels, receiver_descriptions, receiver_ids, connectable_receiver_indices, include_connection_api=True)
+        # Register 'not connectable receviers
+        receiver_ids = self._register_resources("receiver", receiver_labels, receiver_descriptions, receiver_ids, not_connectable_receiver_indices, include_connection_api=False)
 
-        self.receiver_expected_answers = self._generate_answers(receiver_labels, receiver_descriptions, receiver_ids, random_indices)
+        self.receiver_expected_answers = self._generate_answers(receiver_labels, receiver_descriptions, receiver_ids, receiver_indices)
         self.receiver_possible_answers = self._generate_answers(receiver_labels, receiver_descriptions, receiver_ids, range(len(receiver_labels)))
+        self.connectable_receiver_expected_answers = self._generate_answers(receiver_labels, receiver_descriptions, receiver_ids, connectable_receiver_indices);
 
     def load_resource_data(self):
         """Loads test data from files"""
@@ -370,7 +379,7 @@ class JTNMTest(GenericTest):
             data = self.test_data[type]
 
         if not reg_url:
-            reg_url = self.registry_location + 'x-nmos/registration/v1.3/'
+            reg_url = self.mock_registry_base_url + 'x-nmos/registration/v1.3/'
 
         if not codes:
             codes = [200, 201]
@@ -407,7 +416,7 @@ class JTNMTest(GenericTest):
 
         return location, timestamp
 
-    def post_super_resources_and_resource(self, test, type, description, sender_id=None, receiver_id=None, fail=Test.FAIL):
+    def post_super_resources_and_resource(self, test, type, description, include_connection_api=True, sender_id=None, receiver_id=None, fail=Test.FAIL):
         """
         Perform POST requests on the Registration API to create the super-resource registrations
         for the requested type, before performing a POST request to create that resource registration
@@ -420,29 +429,35 @@ class JTNMTest(GenericTest):
         if type == "node":
             pass
         elif type == "device":
-            node = self.post_super_resources_and_resource(test, "node", description, sender_id, receiver_id, fail=Test.UNCLEAR)
+            node = self.post_super_resources_and_resource(test, "node", description, include_connection_api, sender_id, receiver_id, fail=Test.UNCLEAR)
             data["node_id"] = node["id"]
+            if include_connection_api:
+                # Update the controls data with the URL of the mock node
+                controls = data["controls"][0]["href"] = self.mock_node_base_url + '/x-nmos/connection/v1.0/'
+            else:
+                data["controls"] = [] # Remove controls data
             data["senders"] = [ sender_id ] if sender_id else [] 
             data["receivers"] = [ receiver_id ] if receiver_id else [] 
         elif type == "source":
-            device = self.post_super_resources_and_resource(test, "device", description, sender_id, receiver_id, fail=Test.UNCLEAR)
+            device = self.post_super_resources_and_resource(test, "device", description, include_connection_api, sender_id, receiver_id, fail=Test.UNCLEAR)
             data["device_id"] = device["id"]
         elif type == "flow":
-            source = self.post_super_resources_and_resource(test, "source", description, sender_id, receiver_id, fail=Test.UNCLEAR)
+            source = self.post_super_resources_and_resource(test, "source", description, include_connection_api, sender_id, receiver_id, fail=Test.UNCLEAR)
             data["device_id"] = source["device_id"]
             data["source_id"] = source["id"]
             # since device_id is v1.1, downgrade
-            data = self.downgrade_resource(type, data, self.apis[REG_API_KEY]["version"])
+            # Hmm We need to specify the registry version to ensure we downgrade to the right version
+            #data = NMOSUtils.downgrade_resource(type, data, self.apis[REG_API_KEY]["version"])
         elif type == "sender":
             sender_id = str(uuid.uuid4())
             data["id"] = sender_id
-            device = self.post_super_resources_and_resource(test, "device", description, sender_id, receiver_id, fail=Test.UNCLEAR)
-            data["device_id"] = device["id"]
-            data["flow_id"] = str(uuid.uuid4())  # or post a flow first and use its id here?
+            flow = self.post_super_resources_and_resource(test, "flow", description, include_connection_api, sender_id, receiver_id, fail=Test.UNCLEAR)
+            data["device_id"] = flow["device_id"]
+            data["flow_id"] = flow["id"]  # or post a flow first and use its id here?
         elif type == "receiver":
             receiver_id = str(uuid.uuid4())
             data["id"] = receiver_id
-            device = self.post_super_resources_and_resource(test, "device", description, sender_id, receiver_id, fail=Test.UNCLEAR)
+            device = self.post_super_resources_and_resource(test, "device", description, include_connection_api, sender_id, receiver_id, fail=Test.UNCLEAR)
             data["device_id"] = device["id"]
 
         self.post_resource(test, type, data, codes=[201], fail=fail)
@@ -458,7 +473,7 @@ class JTNMTest(GenericTest):
         'Management API.\n\nA Test AMWA IS-04 v1.2/1.3 reference Registry is available on the network, ' \
         'and advertised in the DNS server via unicast DNS-SD\n\n' \
         'Although the test AMWA IS-04 Registry should be discoverable via DNS-SD, for the purposes of developing this testing framework ' \
-        'it is also possible to reach the Registry via the following URL:\n\n' + self.registry_location + 'x-nmos/query/v1.3\n\n' \
+        'it is also possible to reach the Registry via the following URL:\n\n' + self.mock_registry_base_url + 'x-nmos/query/v1.3\n\n' \
         'Once the BCuT has located the test AMWA IS-04 Registry, please click \'Next\''
         possible_answers=[]
 
@@ -579,8 +594,25 @@ class JTNMTest(GenericTest):
         """
         Identify which Receiver devices are controllable via IS-05
         """
+        try:
+            # Check receivers 
+            question = 'Some of the discovered Receivers are controllable via IS-05, for instance, allowing Senders to be connected.\n' \
+            'Carefully select the Receivers that have connection APIs from the following list.'
+            possible_answers = self.receiver_possible_answers
 
-        return test.DISABLED("Test not yet implemented")
+            actual_answers = self._invoke_client_facade(question, possible_answers, test_type="checkbox")
+
+            if len(actual_answers) != len(self.connectable_receiver_expected_answers):
+                return test.FAIL('Incorrect Receiver identified')
+            else:
+                for answer in actual_answers:
+                    if answer not in self.connectable_receiver_expected_answers:
+                        return test.FAIL('Incorrect Receiver identified')
+
+            return test.PASS('All Receivers correctly identified')
+        except ClientFacadeException as e:
+            return test.UNCLEAR(e.args[0])
+
 
     def test_07(self, test):
         """
